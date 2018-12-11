1e847741d701bcf46d87c2b05c07154c0f339496
chadmckell <32710340+chadmckell@users.noreply.github.com>
2018-12-11 10:40:53 -0500

    Update PluginProcessor.cpp

--- C++/PluginProcessor.cpp
+++ C++/PluginProcessor.cpp
@@ -10,17 +10,19 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
-#include "SinewaveSynth.h"
+#include "SquarewaveSynth.h"
 #include <iostream>
 #include <math.h>
+#include </usr/local/Eigen/Dense>
 
 using namespace std;
+using namespace Eigen;
 
 AudioProcessor* JUCE_CALLTYPE createPluginFilter();
 
 
 //==============================================================================
-MoogVcftrapAudioProcessor::MoogVcftrapAudioProcessor()
+MoogVcftrapNlAudioProcessor::MoogVcftrapNlAudioProcessor()
      : AudioProcessor (getBusesProperties())
 {
     lastPosInfo.resetToDefault();
@@ -31,15 +33,16 @@ MoogVcftrapAudioProcessor::MoogVcftrapAudioProcessor()
     addParameter (gainParam  = new AudioParameterFloat ("gain",  "Overall Gain", 0.0f, 2.0f, 1.4f));
     addParameter (cutParam  = new AudioParameterFloat ("cutoff",  "Cutoff", 0.0f, 10000.0f, 10000.0f));
     addParameter (resParam  = new AudioParameterFloat ("resonance",  "Resonance", 0.0f, 1.0f, 0.0f));
+    addParameter (rateParam  = new AudioParameterFloat ("rate",  "Rate", 0.0f, 10.0f, 0.0f));
 
     initialiseSynth();
 }
 
-MoogVcftrapAudioProcessor::~MoogVcftrapAudioProcessor()
+MoogVcftrapNlAudioProcessor::~MoogVcftrapNlAudioProcessor()
 {
 }
 
-void MoogVcftrapAudioProcessor::initialiseSynth()
+void MoogVcftrapNlAudioProcessor::initialiseSynth()
 {
     const int numVoices = 8;
     
@@ -52,7 +55,7 @@ void MoogVcftrapAudioProcessor::initialiseSynth()
 }
 
 //==============================================================================
-bool MoogVcftrapAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool MoogVcftrapNlAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 {
     // Only mono/stereo and input/output must have same layout
     const AudioChannelSet& mainOutput = layouts.getMainOutputChannelSet();
@@ -73,38 +76,39 @@ bool MoogVcftrapAudioProcessor::isBusesLayoutSupported (const BusesLayout& layou
     return true;
 }
 
-AudioProcessor::BusesProperties MoogVcftrapAudioProcessor::getBusesProperties()
+AudioProcessor::BusesProperties MoogVcftrapNlAudioProcessor::getBusesProperties()
 {
     return BusesProperties().withInput  ("Input",  AudioChannelSet::stereo(), true)
     .withOutput ("Output", AudioChannelSet::stereo(), true);
 }
 
 //==============================================================================
-void MoogVcftrapAudioProcessor::prepareToPlay (double newSampleRate, int /*samplesPerBlock*/)
+void MoogVcftrapNlAudioProcessor::prepareToPlay (double newSampleRate, int /*samplesPerBlock*/)
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
     synth.setCurrentPlaybackSampleRate (newSampleRate);
     keyboardState.reset();
+    previousGain = *gainParam;
     
     reset();
 }
 
-void MoogVcftrapAudioProcessor::releaseResources()
+void MoogVcftrapNlAudioProcessor::releaseResources()
 {
     // When playback stops, you can use this as an opportunity to free up any
     // spare memory, etc.
     keyboardState.reset();
 }
 
-void MoogVcftrapAudioProcessor::reset()
+void MoogVcftrapNlAudioProcessor::reset()
 {
     // Use this method as the place to clear any delay lines, buffers, etc, as it
     // means there's been a break in the audio's continuity.
 }
 
 template <typename FloatType>
-void MoogVcftrapAudioProcessor::process (AudioBuffer<FloatType>& buffer,
+void MoogVcftrapNlAudioProcessor::process (AudioBuffer<FloatType>& buffer,
                                            MidiBuffer& midiMessages)
 {
     const int numSamples = buffer.getNumSamples();
@@ -128,138 +132,40 @@ void MoogVcftrapAudioProcessor::process (AudioBuffer<FloatType>& buffer,
     // apply the gain-change to the outgoing data
     applyGain (buffer);
     
-    // Now ask the host for the current time so we can store it to be displayed later
+    // Now ask the host for the current time so we can store it to be displayed later...
     updateCurrentTimeInfoFromHost();
 }
 
 template <typename FloatType>
-void MoogVcftrapAudioProcessor::applyGain (AudioBuffer<FloatType>& buffer)
+void MoogVcftrapNlAudioProcessor::applyGain (AudioBuffer<FloatType>& buffer)
 {
-    const float gainLevel = *gainParam;
+    // Use linear interpolation to determine gainLevel. Note that if you don't do interpolation you could produce some unwanted audio clips in your output signal (i.e. discontinuities) because your gain level parameter updates at a rate much faster than your sampling rate.
+
+    auto currentGain = gainParam->get();
     
     for (int channel = 0; channel < getTotalNumOutputChannels(); ++channel)
-        buffer.applyGain (channel, 0, buffer.getNumSamples(), gainLevel);
+        
+        if (currentGain == previousGain)
+        {
+            buffer.applyGain (channel, 0, buffer.getNumSamples(), currentGain);
+        }
+        else
+        {
+            buffer.applyGainRamp(channel, 0, buffer.getNumSamples(), previousGain, currentGain);
+            previousGain = currentGain;
+        }
 }
 
 template <typename FloatType>
-void MoogVcftrapAudioProcessor::applyMoog (AudioBuffer<FloatType>& buffer)
+void MoogVcftrapNlAudioProcessor::applyMoog (AudioBuffer<FloatType>& buffer)
 {
-    // Define parameters
-    float f0 = *cutParam; // cut off frequency
-    float r = *resParam; // tuning parameter, i.e. "resonance" (a number between 0 and 1)
+    // Set constant parameters
     const float pi = MathConstants<double>::pi; // constant value of pi
     float SR = getSampleRate(); // sample rate [samples/sec]
     float k = 1/SR; // time step [sec]
-    float w0 = 2*pi*f0; // angular frequency [rad/sec]
-    
-    float A[4][4] = { { -w0,  0,   0, -4*r*w0 },
-        {  w0, -w0,  0,       0 },
-        {   0,  w0, -w0,      0 },
-        {   0,   0,  w0,    -w0 } }; // system matrix
-    
-    float b[4] = {w0,0,0,0}; // forcing vector
-    float eye[4][4] = { { 1, 0, 0, 0 },
-        { 0, 1, 0, 0 },
-        { 0, 0, 1, 0 },
-        { 0, 0, 0, 1 } }; // identity matrix
-    
-    // Compute coefficients
-    float e[4][4];
-    float f[4][4];
-    
-    for (int n = 0; n < 4; n++)
-    {
-        for (int m = 0; m < 4; m++)
-        {
-            e[n][m] = eye[n][m] + k*A[n][m]/2;
-            f[n][m] = eye[n][m] - k*A[n][m]/2;
-        }
-    }
-    
-    // Find the determinant of f
-    float det = f[0][0]*(f[1][1]*f[2][2]*f[3][3] + f[1][2]*f[2][3]*f[3][1]
-                         + f[1][3]*f[2][1]*f[3][2] - f[1][1]*f[2][3]*f[3][2]
-                         - f[1][2]*f[2][1]*f[3][3] - f[1][3]*f[2][2]*f[3][1])
-    + f[0][1]*(f[1][0]*f[2][3]*f[3][2] + f[1][2]*f[2][0]*f[3][3]
-               + f[1][3]*f[2][2]*f[3][0] - f[1][0]*f[2][2]*f[3][3]
-               - f[1][2]*f[2][3]*f[3][0] - f[1][3]*f[2][0]*f[3][2])
-    + f[0][2]*(f[1][0]*f[2][1]*f[3][3] + f[1][1]*f[2][3]*f[3][0]
-               + f[1][3]*f[2][0]*f[3][1] - f[1][0]*f[2][3]*f[3][1]
-               - f[1][1]*f[2][0]*f[3][3] - f[1][3]*f[2][1]*f[3][0])
-    + f[0][3]*(f[1][0]*f[2][2]*f[3][1] + f[1][1]*f[2][0]*f[3][2]
-               + f[1][2]*f[2][1]*f[3][0] - f[1][0]*f[2][1]*f[3][2]
-               - f[1][1]*f[2][2]*f[3][0] - f[1][2]*f[2][0]*f[3][1]);
-
-    // Compute inverse of f
-    float invDET = 1.0f/det;
-    float invF[4][4];
-
-    invF[0][0] = invDET*(f[1][1]*f[2][2]*f[3][3] + f[1][2]*f[2][3]*f[3][1] +
-                         f[1][3]*f[2][1]*f[3][2] - f[1][1]*f[2][3]*f[3][2] -
-                         f[1][2]*f[2][1]*f[3][3] - f[1][3]*f[2][2]*f[3][1]);
-
-    invF[0][1] = invDET*(f[0][1]*f[2][3]*f[3][2] + f[0][2]*f[2][1]*f[3][3] +
-                         f[0][3]*f[2][2]*f[3][1] - f[0][1]*f[2][2]*f[3][3] -
-                         f[0][2]*f[2][3]*f[3][1] - f[0][3]*f[2][1]*f[3][2]);
-
-    invF[0][2] = invDET*(f[0][2]*f[1][3]*f[3][1] - f[0][3]*f[1][2]*f[3][1] +
-                         f[0][3]*f[1][1]*f[3][2] - f[0][1]*f[1][3]*f[3][2] -
-                         f[0][2]*f[1][1]*f[3][3] + f[0][1]*f[1][2]*f[3][3]);
-
-    invF[0][3] = invDET*(f[0][3]*f[1][2]*f[2][1] - f[0][2]*f[1][3]*f[2][1] -
-                         f[0][3]*f[1][1]*f[2][2] + f[0][1]*f[1][3]*f[2][2] +
-                         f[0][2]*f[1][1]*f[2][3] - f[0][1]*f[1][2]*f[2][3]);
-
-    invF[1][0] = invDET*(f[1][0]*f[2][3]*f[3][2] + f[1][2]*f[2][0]*f[3][3] +
-                         f[1][3]*f[2][2]*f[3][0] - f[1][0]*f[2][2]*f[3][3] -
-                         f[1][2]*f[2][3]*f[3][0] - f[1][3]*f[2][0]*f[3][2]);
-
-    invF[1][1] = invDET*(f[0][2]*f[2][3]*f[3][0] - f[0][3]*f[2][2]*f[3][0] +
-                         f[0][3]*f[2][0]*f[3][2] - f[0][0]*f[2][3]*f[3][2] -
-                         f[0][2]*f[2][0]*f[3][3] + f[0][0]*f[2][2]*f[3][3]);
-
-    invF[1][2] = invDET*(f[0][3]*f[1][2]*f[3][0] - f[0][2]*f[1][3]*f[3][0] -
-                         f[0][3]*f[1][0]*f[3][2] + f[0][0]*f[1][3]*f[3][2] +
-                         f[0][2]*f[1][0]*f[3][3] - f[0][0]*f[1][2]*f[3][3]);
-
-    invF[1][3] = invDET*(f[0][2]*f[1][3]*f[2][0] - f[0][3]*f[1][2]*f[2][0] +
-                         f[0][3]*f[1][0]*f[2][2] - f[0][0]*f[1][3]*f[2][2] -
-                         f[0][2]*f[1][0]*f[2][3] + f[0][0]*f[1][2]*f[2][3]);
-
-    invF[2][0] = invDET*(f[1][0]*f[2][1]*f[3][3] + f[1][1]*f[2][3]*f[3][0] +
-                         f[1][3]*f[2][0]*f[3][1] - f[1][0]*f[2][3]*f[3][1] -
-                         f[1][1]*f[2][0]*f[3][3] - f[1][3]*f[2][1]*f[3][0]);
-
-    invF[2][1] = invDET*(f[0][3]*f[2][1]*f[3][0] - f[0][1]*f[2][3]*f[3][0] -
-                         f[0][3]*f[2][0]*f[3][1] + f[0][0]*f[2][3]*f[3][1] +
-                         f[0][1]*f[2][0]*f[3][3] - f[0][0]*f[2][1]*f[3][3]);
-
-    invF[2][2] = invDET*(f[0][1]*f[1][3]*f[3][0] - f[0][3]*f[1][1]*f[3][0] +
-                         f[0][3]*f[1][0]*f[3][1] - f[0][0]*f[1][3]*f[3][1] -
-                         f[0][1]*f[1][0]*f[3][3] + f[0][0]*f[1][1]*f[3][3]);
-
-    invF[2][3] = invDET*(f[0][3]*f[1][1]*f[2][0] - f[0][1]*f[1][3]*f[2][0] -
-                         f[0][3]*f[1][0]*f[2][1] + f[0][0]*f[1][3]*f[2][1] +
-                         f[0][1]*f[1][0]*f[2][3] - f[0][0]*f[1][1]*f[2][3]);
-
-    invF[3][0] = invDET*(f[1][0]*f[2][2]*f[3][1] + f[1][1]*f[2][0]*f[3][2] +
-                         f[1][2]*f[2][1]*f[3][0] - f[1][0]*f[2][1]*f[3][2] -
-                         f[1][1]*f[2][2]*f[3][0] - f[1][2]*f[2][0]*f[3][1]);
-
-    invF[3][1] = invDET*(f[0][1]*f[2][2]*f[3][0] - f[0][2]*f[2][1]*f[3][0] +
-                         f[0][2]*f[2][0]*f[3][1] - f[0][0]*f[2][2]*f[3][1] -
-                         f[0][1]*f[2][0]*f[3][2] + f[0][0]*f[2][1]*f[3][2]);
-
-    invF[3][2] = invDET*(f[0][2]*f[1][1]*f[3][0] - f[0][1]*f[1][2]*f[3][0] -
-                         f[0][2]*f[1][0]*f[3][1] + f[0][0]*f[1][2]*f[3][1] +
-                         f[0][1]*f[1][0]*f[3][2] - f[0][0]*f[1][1]*f[3][2]);
-
-    invF[3][3] = invDET*(f[0][1]*f[1][2]*f[2][0] - f[0][2]*f[1][1]*f[2][0] +
-                         f[0][2]*f[1][0]*f[2][1] - f[0][0]*f[1][2]*f[2][1] -
-                         f[0][1]*f[1][0]*f[2][2] + f[0][0]*f[1][1]*f[2][2]);
     
     const int numSamples = buffer.getNumSamples();
-    
+
     for (int i = 0; i < numSamples; ++i)
     {
         // Define signals
@@ -267,32 +173,66 @@ void MoogVcftrapAudioProcessor::applyMoog (AudioBuffer<FloatType>& buffer)
         auto leftBuffer  = buffer.getWritePointer (0); // left output
         auto rightBuffer = buffer.getWritePointer (1); // right output
         auto in = channelData[i];
-
-        // Initialize variables
-        float d1[4];
-        float d2[4];
-        float d[4];
         
-        // Compute output signal
-        for (int j = 0; j < 4; j++)
+        // Set variable parameters
+        float rate = *rateParam; // LFO rate [Hz]
+        float fc = *cutParam + 1000*(0.5 + 0.5*cos(2*pi*rate*(count + i)/SR)); // cut off frequency [Hz]
+        float wc = 2*pi*fc; // angular cut off frequency [rad/sec]
+        float r = *resParam;// + sidechain[i]; // resonance (with sidechain modulation)
+        
+        float A[4][4] = { { -wc,  0,   0, -4*r*wc },
+            {  wc, -wc,  0,       0 },
+            {   0,  wc, -wc,      0 },
+            {   0,   0,  wc,    -wc } }; // system matrix
+        
+        float b[4] = {wc,0,0,0}; // forcing vector
+        float eye[4][4] = { { 1, 0, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 0, 1, 0 },
+            { 0, 0, 0, 1 } }; // identity matrix
+        
+        // Compute matrix terms
+        Matrix4f alpha;
+        Matrix4f beta;
+        
+        for (int n = 0; n < 4; n++)
         {
-            d1[j] = e[j][0]*x[0] + e[j][1]*x[1] + e[j][2]*x[2] + e[j][3]*x[3];
-            d2[j] = k*in*(e[j][0]*b[0] + e[j][1]*b[1] + e[j][2]*b[2] + e[j][3]*b[3]);
-            d[j] = d1[j] + d2[j];
+            for (int m = 0; m < 4; m++)
+            {
+                alpha(n,m) = eye[n][m] + k*A[n][m]/2;
+                beta(n,m) = eye[n][m] - k*A[n][m]/2;
+            }
         }
-            
+        
+        // Compute beta inverse
+        Matrix4f invBeta = beta.inverse();
+        
+        // Calculate gamma
+        float gamma1[4];
+        float gamma2[4];
+        float gamma[4];
+        
         for (int j = 0; j < 4; j++)
         {
-            x[j] = invF[j][0]*d[0] + invF[j][1]*d[1] + invF[j][2]*d[2] + invF[j][3]*d[3];
+            gamma1[j] = alpha(j,0)*x[0] + alpha(j,1)*x[1] + alpha(j,2)*x[2] + alpha(j,3)*x[3];
+            gamma2[j] = k*in*(alpha(j,0)*b[0] + alpha(j,1)*b[1] + alpha(j,2)*b[2] + alpha(j,3)*b[3]);
+            gamma[j] = gamma1[j] + gamma2[j];
+        }
+        
+        // Compute state update
+        for (int j = 0; j < 4; j++)
+        {
+            x[j] = invBeta(j,0)*gamma[0] + invBeta(j,1)*gamma[1] + invBeta(j,2)*gamma[2] + invBeta(j,3)*gamma[3];
         }
         
         // Write output signal to left and right channels
         leftBuffer[i]  = x[3];
         rightBuffer[i] = x[3];
     }
+    count += numSamples;
 }
 
-void MoogVcftrapAudioProcessor::updateCurrentTimeInfoFromHost()
+void MoogVcftrapNlAudioProcessor::updateCurrentTimeInfoFromHost()
 {
     if (AudioPlayHead* ph = getPlayHead())
     {
@@ -310,13 +250,13 @@ void MoogVcftrapAudioProcessor::updateCurrentTimeInfoFromHost()
 }
     
 //==============================================================================
-AudioProcessorEditor* MoogVcftrapAudioProcessor::createEditor()
+AudioProcessorEditor* MoogVcftrapNlAudioProcessor::createEditor()
 {
-    return new MoogVcftrapAudioProcessorEditor (*this);
+    return new MoogVcftrapNlAudioProcessorEditor (*this);
 }
     
 //==============================================================================
-void MoogVcftrapAudioProcessor::getStateInformation (MemoryBlock& destData)
+void MoogVcftrapNlAudioProcessor::getStateInformation (MemoryBlock& destData)
 {
     // You should use this method to store your parameters in the memory block.
     // Here's an example of how you can use XML to make it easy and more robust:
@@ -337,7 +277,7 @@ void MoogVcftrapAudioProcessor::getStateInformation (MemoryBlock& destData)
     copyXmlToBinary (xml, destData);
 }
     
-void MoogVcftrapAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void MoogVcftrapNlAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
@@ -362,11 +302,11 @@ void MoogVcftrapAudioProcessor::setStateInformation (const void* data, int sizeI
     }
 }
     
-void MoogVcftrapAudioProcessor::updateTrackProperties (const TrackProperties& properties)
+void MoogVcftrapNlAudioProcessor::updateTrackProperties (const TrackProperties& properties)
 {
     trackProperties = properties;
         
-    if (auto* editor = dynamic_cast<MoogVcftrapAudioProcessorEditor*> (getActiveEditor()))
+    if (auto* editor = dynamic_cast<MoogVcftrapNlAudioProcessorEditor*> (getActiveEditor()))
         editor->updateTrackProperties ();
 }
     
@@ -374,7 +314,7 @@ void MoogVcftrapAudioProcessor::updateTrackProperties (const TrackProperties& pr
 // This creates new instances of the plugin..
 AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return new MoogVcftrapAudioProcessor();
+    return new MoogVcftrapNlAudioProcessor();
 }
     
     
